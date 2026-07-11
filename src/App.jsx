1a18029d24d07feaefeccca6b23b52c00a53c401
sridhar-hp <sridhark3773@gmail.com>
2026-07-11 11:04:51 +0530

    Analytics page

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ import GitHub from "./pages/GithubPage/GitHub";
 import LeetCode from "./pages/LeetCode/LeetCode";
 import Learning from "./pages/Learning/Learning";
 import Communication from "./pages/Communication/Communication";
-import LinkedIn from "./pages/LinkedIn";
-// import Analytics from "./pages/Analytics";
+import LinkedIn from "./pages/Linkedin/LinkedIn";
+import Analytics from "./pages/Analytics/Analytics";
 // import Journal from "./pages/Journal";
 // import Reports from "./pages/Reports";
 // import Achievements from "./pages/Achievements";
@@ -42,9 +42,9 @@ export default function App() {
                     <Route path="/learning" element={<Learning />} />
                     <Route path="/communication" element={<Communication />} />
                     <Route path="/linkedin" element={<LinkedIn />} />
+          <Route path="/analytics" element={<Analytics />} />
 
                     {/*  
-          <Route path="/analytics" element={<Analytics />} />
           <Route path="/journal" element={<Journal />} />
           <Route path="/reports" element={<Reports />} />
           <Route path="/achievements" element={<Achievements />} />
